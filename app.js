95965152adf7942624732bb32b163f39216687a3
ahsan <ahsanraza85858585@gmail.com>
2024-07-14 08:11:41 -0700

    add new js

--- app.js
+++ app.js
@@ -1,27 +1,25 @@
-/*function  sallery(x) {
-    for( let x=1; x> 100;x++){
-        let sall= x*1200;
-        return sall;
+let person={
+    name:"ahsan",
+    year:"forth_semeter",
+    Roll_non:"12312",
+setYear(saal){
+    this.year=saal;
+    
+},
+
+};
+console.log(this.year);
+/*let person = {
+    name: "ahsan",
+    year: "forth_semeter",
+    Roll_non: "12312",
+    setYear(saal) {
+      this.year = saal;
+      console.log(this.year);
     }
-console.log(sallery(4));
-}*/
-function sallery(x) {
-    let basic_pay = 1300;
-    let sall = x * basic_pay;
-    return sall;
-}
-
-console.log(sallery(5)); // This will log 6500 (5 * 1300)
-function sallery() {
-    let salaries = [];
-    let basic_pay = 1300;
-
-    for (let x = 0; x < 3000; x++) {
-        let sall = x * basic_pay;
-        salaries.push(sall);
-    }
-
-    return salaries;
-}
-
-console.log(sallery()); // This will log an array of salaries
+  };
+  
+  console.log(person.year); // Output: "forth_semeter"
+  
+  person.setYear("fifth_semeter");
+  console.log(person.year); // Output: "fifth_semeter"*/
