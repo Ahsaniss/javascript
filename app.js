756fc5593bb4251778d5b3a63c0a4c5c058005b7
ahsan <ahsanraza85858585@gmail.com>
2024-07-15 01:10:04 -0700

    function in string

--- app.js
+++ app.js
@@ -23,7 +23,28 @@ console.log(this.year);
   
   person.setYear("fifth_semeter");
   console.log(person.year); // Output: "fifth_semeter"*/
-  var a=new Date();
+ /* var a=new Date();
   d=a.getFullYear();
 
   console.log('date is today' + " "+(d))
+  let students={
+    NAME:"AHSAN",
+    CLASS:"FORTH_SEMESTER",
+
+  };
+  let out_put=Object.keys(students).map(function(e){
+    return students[e]
+  })
+  console.log(out_put);
+   console.log(students);*/
+   let boy={
+    name:"ahsan",
+    Roll_no:222016,
+    full_name:function(){
+      return this.full_name + "and" +this.Roll_no;
+    }
+  
+   };
+
+ 
+  
